54638fb55ce7a6f736d8dc45b655f4a287b52f7f
Javier Arguello <jas91@bu.edu>
2017-02-16 16:57:42 -0500

    Modified start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-git submodule init && git submodule update
-vim +PluginInstall +qall
-ln -s ~/.vim/vimrc ~/.vimrc 2> /dev/null
+git submodule init && git submodule update && vim +PluginInstall +qall && ln -s ~/.vim/vimrc ~/.vimrc
